486c6ffeb8152e9fdbaff463701837b98324a622
shioncm <shioncm2001@gmail.com>
2023-02-08 23:41:30 -0500

    Attempt to add keyboard interrupt detection - its not working lol

--- main.py
+++ main.py
@@ -19,28 +19,36 @@ while True:
     connectionSocket, addr = serverSocket.accept() 
     
     try:
-        message = connectionSocket.recv(1024)
+        message = connectionSocket.recv(4096)
         filename = message.split()[1] 
+        print(filename + '\n')
         f = open(filename[1:]) 
         outputdata = f.read()
  
         #Send one HTTP header line into socket
-        connectionSocket.send(b'HTTP/1.1 200 OK\r\n\r\n')
+        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n')
  
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)): 
             connectionSocket.send(outputdata[i].encode())
         
         connectionSocket.send("\r\n".encode())
+        print('Success!')
 
+        #Close client socket
         connectionSocket.close()
- 
     except IOError:
         #Send response message for file not found
-        connectionSocket.send(b'HTTP/1.1 404 Not found\r\n\r\n')
+        connectionSocket.send('HTTP/1.1 404 Not found\r\n\r\n')
+        connectionSocket.send("<html><head></head><body><h1>404 Not found</h1></body></html>\r\n")
         
         #Close client socket
         connectionSocket.close()
+    except KeyboardInterrupt:
+        print('Keyboard interrupt detected')
+        connectionSocket.close()
+        serverSocket.close()
+        sys.exit()
 
 serverSocket.close()
 sys.exit() #Terminate the program after sending the corresponding data
