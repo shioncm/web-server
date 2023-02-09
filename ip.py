c6dc103636dc92adcd405e2cd21a08d9f3fcf051
shioncm <shioncm2001@gmail.com>
2023-02-09 14:43:38 -0500

    Maybe finished

--- ip.py
+++ ip.py
@@ -1,5 +1,11 @@
 ## importing socket module
 import socket
 ## getting the hostname by socket.gethostname() method
-ip_address = socket.gethostbyname(socket.gethostname())
+ip_address = socket.gethostbyname("localhost")
 print(f"IP Address: {ip_address}") #127.0.1.1
+
+host = socket.gethostname()
+print(f"IP Address 2: {host}")
+
+ip2 = socket.gethostbyname(socket.gethostname())
+print(f"IP Address 3: {ip2}")
--- webserver.py
+++ webserver.py
@@ -1,32 +1,29 @@
 #import socket module
 from socket import * 
-# import socket
 import sys # In order to terminate the program
 
-# print(socket.gethostbyname(socket.gethostname()))
-
 serverSocket = socket(AF_INET, SOCK_STREAM)
-# serverSocket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
 #Prepare a sever socket
-serverport = 6787
-serverSocket.bind(('', serverport))
-serverSocket.listen(5)
+serverport = 6789
+serverSocket.bind(("", serverport))
+serverSocket.listen(1)
 
 while True:
     #Establish the connection
     print('Ready to serve...')
     connectionSocket, addr = serverSocket.accept() 
-    
+    print('Connected with ' + addr[0] + ':'
+      + str(addr[1]))
+
     try:
-        message = connectionSocket.recv(4096)
+        message = connectionSocket.recv(1024)
         filename = message.split()[1] 
-        print(filename + '\n')
         f = open(filename[1:]) 
         outputdata = f.read()
  
         #Send one HTTP header line into socket
-        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n')
+        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n'.encode())
  
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)): 
@@ -39,8 +36,8 @@ while True:
         connectionSocket.close()
     except IOError:
         #Send response message for file not found
-        connectionSocket.send('HTTP/1.1 404 Not found\r\n\r\n')
-        connectionSocket.send("<html><head></head><body><h1>404 Not found</h1></body></html>\r\n")
+        connectionSocket.send('HTTP/1.1 404 Not found\r\n\r\n'.encode())
+        connectionSocket.send("<html><head></head><body><h1>404 Not found</h1></body></html>\r\n".encode())
         
         #Close client socket
         connectionSocket.close()
