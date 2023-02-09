6343b789840548628e8f427ff994773a9f2e44c1
shioncm <shioncm2001@gmail.com>
2023-02-08 21:41:50 -0500

    Add index.html, nearly finish main.py

--- main.py
+++ main.py
@@ -6,40 +6,37 @@ serverSocket = socket(AF_INET, SOCK_STREAM)
 
 #Prepare a sever socket
 serverport = 80
-serversocket.bind((socket.gethostname(), serverport))
-serversocket.listen(5)
+serverSocket.bind((socket.gethostname(), serverport))
+serverSocket.listen(5)
 
 while True:
     #Establish the connection
     print('Ready to serve...')
-    connectionSocket, addr = serversocket.accept() 
+    connectionSocket, addr = serverSocket.accept() 
     
     try:
         message = connectionSocket.recv(1024)
         filename = message.split()[1] 
         f = open(filename[1:]) 
-        outputdata = #Fill in start #Fill in end 
+        outputdata = f.read()
  
         #Send one HTTP header line into socket
-        #Fill in start
-        #Fill in end 
+        connectionSocket.send('HTTP/1.1 200 OK\r\n\r\n')
  
         #Send the content of the requested file to the client
         for i in range(0, len(outputdata)): 
             connectionSocket.send(outputdata[i].encode())
         
         connectionSocket.send("\r\n".encode())
- 
+
         connectionSocket.close()
  
     except IOError:
         #Send response message for file not found
-        #Fill in start 
-        #Fill in end
+        connectionSocket.send('\nHTTP/1.1 404 Not Found\n\n')
         
         #Close client socket
-        #Fill in start
-        #Fill in end 
+        connectionSocket.close()
 
 serverSocket.close()
 sys.exit() #Terminate the program after sending the corresponding data
